d2fe0c67f3869c0a188916e260802e909ba21c52
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-09 10:37:43 +0530

    Update

--- Linq Delegate/Linq Delegate/Program.cs
+++ Linq Delegate/Linq Delegate/Program.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace Linq_Delegate
 {
-    delegate bool FindStudent(FindStudent std);
+    delegate bool FindStudent(Student std);
+
     class StudentExtension
     {
-        public static StudentExtension[] where(Student[] stdArray, FindStudent del)
+        public static Student[] where(Student[] stdArray, FindStudent del)
         {
             int i = 0;
             Student[] result = new Student[10];
-            foreach(Student std in stdArray)
+            foreach (Student std in stdArray)
             {
                 if (del(std))
                 {
@@ -22,36 +23,33 @@ namespace Linq_Delegate
                 }
                 return result;
             }
+            return result;
         }
     }
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Student[] studentArray=
+            IList<Student> Studentlist = new List<Student>()
             {
-                new Student(){ StudentID=1,StudentName="John",Age=13},
-                new Student(){ StudentID=2,StudentName="Moin",Age=21},
-                new Student(){StudentID=3,StudentName="Bill",Age=18},
-                new Student(){ StudentID=4,StudentName="Ram",Age=20}
+                new Student(){ StudentId = 1, StudentName ="John",Age=13},
+                new Student(){ StudentId = 2, StudentName ="Moin",Age =21},
+                new Student(){ StudentId = 3, StudentName ="Bill",Age = 18},
+                new Student(){ StudentId = 4, StudentName ="Ram",Age = 20},
+                new Student(){ StudentId = 5, StudentName ="Ron", Age = 15}
             };
-            //var teenAgerStudent = from s in studentlist where s.Age > 12 && s.Age < 20 select s;
-            //Console.WriteLine("Teenage Student");
-            //foreach (Student std in teenAgerStudent)
-            //{
-            //    Console.WriteLine(std.StudentName);
-            //}
-            Student[] students = StudentExtension.where(studentArray, delegate (Student std)
-               {
-                   return std.Age > 12 && std.Age < 40;
-               });
-            Console.Read();
+            var teenAgerStudent = from s in Studentlist where s.Age >= 12 && s.Age < 20 select s;
+            Console.WriteLine("Teenage Students Are:");
+            foreach (var str in teenAgerStudent)
+                Console.WriteLine(str.StudentName);
+            Console.ReadLine();
         }
     }
-    public class Student
+    class Student
     {
-        public int StudentID { get; set; }
+        public int StudentId { get; set; }
         public string StudentName { get; set; }
+
         public int Age { get; set; }
     }
 }
