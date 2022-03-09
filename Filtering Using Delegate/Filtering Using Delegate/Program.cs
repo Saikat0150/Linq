24b5e20ba6e60395df04a34f6b05a375c95cb48f
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-09 12:33:30 +0530

    Filtering using Deleget

--- Filtering Using Delegate/Filtering Using Delegate/Program.cs
+++ Filtering Using Delegate/Filtering Using Delegate/Program.cs
@@ -15,15 +15,24 @@ namespace Filtering_Using_Delegate
                 new Student(){ StudentID=1,StudentName="John",Age=13},
                 new Student(){ StudentID=2,StudentName="Moin",Age=21},
                 new Student(){StudentID=3,StudentName="Bill",Age=18},
-                new Student(){ StudentID=4,StudentName="Ram",Age=20}
+                new Student(){ StudentID=4,StudentName="Ram",Age=20},
+                new Student(){ StudentID=5,StudentName="Kiran",Age=25},
+                new Student(){ StudentID=6,StudentName="Sunil",Age=24},
+                new Student(){ StudentID=7,StudentName="Sai",Age=25}
             };
             Func<Student, bool> isTeenAger = delegate (Student s)
                {
                    return s.Age > 12 && s.Age < 20;
                };
 
-            
-            var filteredResult = from s in studentlist where isTeenAger(s) select s;
+
+            //var filteredResult = from s in studentlist where isTeenAger(s) select s;
+            var filteredResult = studentlist.Where((s, i) =>
+                {
+                    if (i % 2 == 0)
+                        return true;
+                    return false;
+                });
             foreach (Student std in filteredResult)
             {
                 Console.WriteLine(std.StudentName);
