352fb80d2f0ead7c9c9fb6905e79d0dd0a76119c
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-09 10:12:07 +0530

    change

---
+++ LinqDemo3/LinqDemo3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Student> studentlist = new List<Student>()
+            {
+                new Student(){ StudentID=1,StudentName="John",Age=13},
+                new Student(){ StudentID=2,StudentName="Moin",Age=21},
+                new Student(){StudentID=3,StudentName="Bill",Age=18},
+                new Student(){ StudentID=4,StudentName="Ram",Age=20}
+            };
+            var teenAgerStudent = from s in studentlist where s.Age > 12 && s.Age < 20 select s;
+            Console.WriteLine("Teenage Student");
+            foreach(Student std in teenAgerStudent)
+            {
+                Console.WriteLine(std.StudentName);
+            }
+            Console.Read();
+        }
+    }
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ demo1/demo1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Bill", "Steve", "James", "Mohan" };
+
+            //LINQ Query
+            var myLinqQuery = from name in names
+                              where name.Contains('a')
+                              select name;
+
+
+            //Query Execution
+            foreach (var name in myLinqQuery)
+                Console.Write(name + " ");
+            Console.ReadLine();
+        }
+    }
+}
