2098afe84a77032e865e80d98d5126d1bd26122f
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-10 17:00:58 +0530

    Generation Operators -- Empty, Range, Repeat

---
+++ Generation Operators/Generation Operators/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generation_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //---Empty Method
+            /*var emptyCollection1 = Enumerable.Empty<string>();
+            var emptyCollection2 = Enumerable.Empty<Student>();
+            Console.WriteLine("Type: " + emptyCollection1.GetType().Name);
+            Console.WriteLine("Count111: " + emptyCollection2.Count());*/
+
+            //---Range Method
+            /*var intCollecttion = Enumerable.Range(10, 10);
+            Console.WriteLine("Total count: " + intCollecttion.Count());
+
+            for(int i = 0; i < intCollecttion.Count(); i++)
+            {
+                Console.WriteLine("Value at index " + intCollecttion.ElementAt(i));
+            }*/
+
+
+            //---Repeat method
+            var intCollecttion = Enumerable.Repeat(10, 10);
+            Console.WriteLine("Total count: " + intCollecttion.Count());
+
+            for (int i = 0; i < intCollecttion.Count(); i++)
+            {
+                Console.WriteLine("Value at index " + intCollecttion.ElementAt(i));
+            }
+            Console.Read();
+        }
+    }
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+    }
+}
