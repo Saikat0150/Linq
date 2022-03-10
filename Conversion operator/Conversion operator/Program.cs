f34386db9dcf84ca9ff4d97e4e66f1745dd9101b
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-10 17:58:28 +0530

    Conversion Operation

---
+++ Conversion operator/Conversion operator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> strList = new List<string>()
+            {
+                "One",
+                "Two",
+                "Three",
+                "Four",
+                "Three"
+            };
+            Console.WriteLine("strList type: " + strList.GetType().Name);
+
+            string[] strArray = strList.ToArray<string>(); //converts list to Array
+
+            Console.WriteLine("strList type: " + strArray.GetType().Name);
+
+            IList<string> newList = strArray.ToList<string>(); //Converts Array to List
+
+            Console.WriteLine("NewList type: " + newList.GetType().Name);
+
+            Console.Read();
+        }
+    }
+}
