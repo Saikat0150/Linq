ae687f8326048ee0d17a53b95d6f24d68452d7aa
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-11 09:41:30 +0530

    Conversion Operators ToDictionary

---
+++ Conversion Operators 1/Conversion Operators 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion_Operators_1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Conversion_Operators1
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                IList<Student> studentList = new List<Student>()
+            {
+                new Student(){ StudentId=1,StudentName="John",Age=18},
+                new Student(){ StudentId=2,StudentName="Steve",Age=21},
+                new Student(){ StudentId=3, StudentName="Bill", Age=18},
+                new Student(){ StudentId=4, StudentName="Ram", Age=20},
+                new Student(){ StudentId=5, StudentName="Ron", Age=21}
+            };
+                IDictionary<int, Student> studentDic = studentList.ToDictionary<Student, int>(s => s.StudentId);
+                foreach (var key in studentDic.Keys)
+                    Console.WriteLine("Key: " + key + " Value: " + (studentDic[key] as Student).StudentName);
+            }
+        }
+        public class Student
+        {
+            public int StudentId { get; set; }
+            public string StudentName { get; set; }
+            public int Age { get; set; }
+        }
+    }
+
+}
